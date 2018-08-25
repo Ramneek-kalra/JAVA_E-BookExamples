cd72a76c3082d057858dfd85214b38d4bf29c2fa
Ramneek Kalra <kalraramneekhmr@gmail.com>
2018-08-26 00:05:02 +0530

    E-Book Examples and Solutions Update

---
+++ src/E_BookExamples/booleanDatatype.java
@@ -0,0 +1,20 @@
+package E_BookExamples;
+
+/**
+ *
+ * @author Ramneek
+ */
+public class booleanDatatype {
+    public static void main(String[] args) {
+        boolean statusCheck = false;
+        int a = 10;
+        int b = 20;
+        //Checking if a < b or not using Conditional
+        //Statements
+        if(a <= b){
+            System.out.println("A is less than B");
+        }else{
+            System.out.println("A is not less than B");
+        }
+    }
+}
